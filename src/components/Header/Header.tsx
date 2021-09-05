2252032af67bc2d845909316e5fef58b8f52c037
Yaroslav Kibysh <yaroslavki@interlink-ua.com>
2021-09-06 01:09:09 +0300

    Add an emoji to the title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">My Contacts App!</Navbar.Brand>
+        <Navbar.Brand href="#home">👥 Contacts</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
