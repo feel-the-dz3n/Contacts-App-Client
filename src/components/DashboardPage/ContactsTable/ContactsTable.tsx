d6f2d997c26ee974260d21dea10bf00f98281850
Yaroslav Kibysh <yaroslavki@interlink-ua.com>
2021-09-12 13:45:34 +0300

    Show spinner when loading

--- src/components/DashboardPage/ContactsTable/ContactsTable.tsx
+++ src/components/DashboardPage/ContactsTable/ContactsTable.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import ContactModel from "../../../models/contact";
 import contactsApi from "../../../services/contacts-api";
 import ContactItem from "../ContactItem/ContactItem";
@@ -20,6 +21,16 @@ export default function ContactsTable() {
   useEffect(() => {
     fetchContacts();
   }, []);
+
+  if (isLoading) {
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
 
   return (
     <div className="ContactsTableContainer">
